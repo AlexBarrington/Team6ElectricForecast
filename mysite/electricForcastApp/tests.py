375930cd78a7bdf7c072bda6c7e29f4662c0d4e2
mbillig <marielle@billig.org>
2015-10-12 22:41:31 -0500

    Merge remote-tracking branch 'origin/mysite' into mysite
    Conflicts:
    	mysite/mydatabase
    	mysite/templates/opAreaIndex.html

--- mysite/electricForcastApp/tests.py
+++ mysite/electricForcastApp/tests.py
@@ -1,12 +1,9 @@
 from django.test import TestCase
 
-"""# Create your tests here.
-class ProjectTests(TestCase):
 
-    def test_homepage(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-"""
+# Create your tests here.
+
+
 from django.contrib.auth import get_user_model
 
 
@@ -17,16 +14,17 @@ class HomePageTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create(username='some_user')
 
-    def test_one_entry(self):
-        Entry.objects.create(title='1-title', body='1-body', author=self.user)
-        response = self.client.get('/')
-        self.assertContains(response, '1-title')
-        self.assertContains(response, '1-body')
+    def test_login_page(self):
+        #response = self.client.get('/admin')
+        response = self.client.get('/admin/login/?next=/admin/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_two_entries(self):
-        Entry.objects.create(title='1-title', body='1-body', author=self.user)
-        Entry.objects.create(title='2-title', body='2-body', author=self.user)
+    def test_dashboard_page(self):
         response = self.client.get('/')
-        self.assertContains(response, '1-title')
-        self.assertContains(response, '1-body')
-        self.assertContains(response, '2-title')
+        self.assertEqual(response.status_code, 302)
+
+    def test_opArea_page(self):
+        response = self.client.get('/OpArea/')
+        self.assertEqual(response.status_code, 200)
+
+
