be75c824e8cdd9f557ff7846804a538b8a0996ca
brittahlgrim <brittany.ahlgrim@marquette.edu>
2015-10-19 21:27:55 -0500

    fixing login site and adding file name reading

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,36 +1,37 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.http import HttpResponse
+from django.template.context_processors import csrf
 from django.template import RequestContext, loader
 from django.conf import settings
 from django.shortcuts import redirect
+from os import listdir
+from os.path import isfile, join
+
 # from django.db.models import Entry
 
-allOpAreas = {
-	"Milwaukee",
-	"Chicago",
-	"Kenosha"
-}
-
+myPath = "C:\\Users\\britt.ahlgrim\\Desktop\\Junior\\Semester 1\\software methodologies\\Project\\data"
+opAreaFiles = [f for f in listdir(myPath) if isfile(join(myPath, f))]
+opAreaNames = [f.replace('.csv', '') for f in opAreaFiles]
 
 def Login(TemplateView):
-	template = loader.get_template('login.html')
-	return HttpResponse(template.render())
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
 
 def HomeView(TemplateView):
-	if not TemplateView.user.is_authenticated():
-		return redirect('http://127.0.0.1:8000/admin/logout')
-	else:
-		template = loader.get_template('index.html')
-		context = RequestContext(TemplateView, {'opAreas': allOpAreas} )
-		return HttpResponse(template.render(context))
-		return HttpResponse(template.render())
+    if not TemplateView.user.is_authenticated():
+        return redirect('http://127.0.0.1:8000/admin/logout')
+    else:
+        c = {'opAreas':opAreaNames}
+        return render(TemplateView, 'index.html', c)
+
 
 def OpAreaView(TemplateView):
-	template = loader.get_template('opAreaIndex.html')
-	context = RequestContext(TemplateView, {'pageTitle': 'OP AREA NAME', 'opAreas': allOpAreas} )
-	return HttpResponse(template.render(context))
-	
+    if not TemplateView.user.is_authenticated():
+        return redirect('http://127.0.0.1:8000/admin/logout')
+    else:
+        c = {'pageTitle': 'OP AREA NAME', 'opAreas': opAreaNames}
+        return render(TemplateView, 'opAreaIndex.html', c)
