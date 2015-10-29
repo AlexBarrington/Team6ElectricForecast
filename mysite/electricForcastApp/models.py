f5d63a897d429fa154d43f68f369770d974fea9e
brittahlgrim <brittany.ahlgrim@marquette.edu>
2015-10-29 18:07:43 -0500

    adding a superuser

--- mysite/electricForcastApp/models.py
+++ mysite/electricForcastApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserModel (models.Model):
+    user = models.ForeignKey(User)
+    username = models.TextField()
+    email = models.TextField()
