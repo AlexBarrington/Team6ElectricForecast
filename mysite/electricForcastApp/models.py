f3c5b3135003af8eb03eedd8a46e67a80b22def1
brittahlgrim <brittany.ahlgrim@marquette.edu>
2015-10-29 19:04:30 -0500

    Adding user model -- working with it to come

--- mysite/electricForcastApp/models.py
+++ mysite/electricForcastApp/models.py
@@ -1,9 +1,80 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
 
-class UserModel (models.Model):
-    user = models.ForeignKey(User)
-    username = models.TextField()
-    email = models.TextField()
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, data_folder, password=None):
+        """
+        Creates and saves a User with the given email, data folder and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            data_folder=data_folder,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, data_folder, password):
+        """
+        Creates and saves a superuser with the given email, data folder and password.
+        """
+        user = self.create_user(email,
+            password=password,
+            data_folder=data_folder
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    data_folder = models.TextField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['data_folder']
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def get_data_folder(self):
+        return self.data_folder
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'electricForcastApp'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,6 +52,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'mysite.urls'
+
+AUTH_USER_MODEL = 'electricForcastApp.MyUser'
 
 TEMPLATES = [
     {
