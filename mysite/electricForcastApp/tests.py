26e184720f348efc4fca25058aa06f27ae3a7161
kswright1212 <kswright1212@gmail.com>
2015-11-01 15:28:38 -0600

    Added additional unit tests

--- mysite/electricForcastApp/tests.py
+++ mysite/electricForcastApp/tests.py
@@ -15,19 +15,32 @@ class HomePageTests(TestCase):
         self.user = get_user_model().objects.create(username='britt.ahlgrim@gmail.com')
         # self.password = get_user_model().objects.create(password='password')
 
-
-
     def test_login_page(self):
         response = self.client.get('/admin/login/?next=/admin/')
         self.assertEqual(response.status_code, 200)
 
     def test_dashboard_page(self):
-        response = self.client.get('/Home/')
+        response = self.client.get('/Dashboard/')
         self.assertEqual(response.status_code, 200)
-
 
     def test_opArea_page(self):
         response = self.client.get('/OpArea/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Chicago(self):
+        response = self.client.get('/OpAreaDetail/BrittChicago/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Denver(self):
+        response = self.client.get('/OpAreaDetail/BrittChicago/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Milwaukee(self):
+        response = self.client.get('/OpAreaDetail/BrittMilwaukee/')
+        self.assertEqual(response.status_code, 200)
+
+    def user_login(self):
+        response = self.client.get('127.0.0.1:8000')
         self.assertEqual(response.status_code, 200)
 
 
